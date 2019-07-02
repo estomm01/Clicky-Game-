1c76db7a34bb701c3f17ad37286fc8164ac85966
Markus Maasikas <markus@hotmail.com>
2019-07-02 18:31:15 -0400

    updated nav

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+
+//imports dependencies and files
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import
+// import Jumbotron from "./components/Jumbotron";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//sets state to 0 or empty
+class App extends Component {
+  state = {
+    cat: "",
+    clickedCat: [],
+    score: 0
+  };
+
+//when you click on a card ... the fish is taken out of the array
+  imageClick = event => {
+    const currentCat = event.target.alt;
+    const CatAlreadyClicked =
+      this.state.clickedCat.indexOf(currentCat) > -1;
+
+//if you click on a fish that has already been selected, the game is reset and cards reordered
+    if (CatAlreadyClicked) {
+      this.setState({
+        cat: this.state.cat.sort(function(a, b) {
+          return 0.5 - Math.random();
+        }),
+        clickedCat: [],
+        score: 0
+      });
+        alert("You lose. Play again?");
+
+//if you click on an available fish, your score is increased and cards reordered
+    } else {
+      this.setState(
+        {
+          cat: this.state.cat.sort(function(a, b) {
+            return 0.5 - Math.random();
+          }),
+          clickedCat: this.state.clickedCat.concat(
+            currentCat
+          ),
+          score: this.state.score + 1
+        },
+//if you get all 12 fish corrent you get a congrats message and the game resets
+        () => {
+          if (this.state.score === 12) {
+            alert("Yay! You Win!");
+            this.setState({
+              cat: this.state.cat.sort(function(a, b) {
+                return 0.5 - Math.random();
+              }),
+              clickedCat: [],
+              score: 0
+            });
+          }
+        }
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Header/>
+        <Container/>
+      </div>
+    )
+  }
 }
 
 export default App;
