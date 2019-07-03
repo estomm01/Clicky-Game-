f2aeb648ac4972f947df7872ebaef561400a5cba
Markus Maasikas <markus@hotmail.com>
2019-07-03 07:35:26 -0400

    updated components

--- src/App.js
+++ src/App.js
@@ -16,28 +16,28 @@ class App extends Component {
     score: 0
   };
 
-//when you click on a card ... the fish is taken out of the array
+  //when you click on a card ... the fish is taken out of the array
   imageClick = event => {
     const currentCat = event.target.alt;
     const CatAlreadyClicked =
       this.state.clickedCat.indexOf(currentCat) > -1;
 
-//if you click on a fish that has already been selected, the game is reset and cards reordered
+    //if you click on a fish that has already been selected, the game is reset and cards reordered
     if (CatAlreadyClicked) {
       this.setState({
-        cat: this.state.cat.sort(function(a, b) {
+        cat: this.state.cat.sort(function (a, b) {
           return 0.5 - Math.random();
         }),
         clickedCat: [],
         score: 0
       });
-        alert("You lose. Play again?");
+      alert("You lose. Play again?");
 
-//if you click on an available fish, your score is increased and cards reordered
+      //if you click on an available fish, your score is increased and cards reordered
     } else {
       this.setState(
         {
-          cat: this.state.cat.sort(function(a, b) {
+          cat: this.state.cat.sort(function (a, b) {
             return 0.5 - Math.random();
           }),
           clickedCat: this.state.clickedCat.concat(
@@ -45,12 +45,12 @@ class App extends Component {
           ),
           score: this.state.score + 1
         },
-//if you get all 12 fish corrent you get a congrats message and the game resets
+        //if you get all 12 fish corrent you get a congrats message and the game resets
         () => {
           if (this.state.score === 12) {
             alert("Yay! You Win!");
             this.setState({
-              cat: this.state.cat.sort(function(a, b) {
+              cat: this.state.cat.sort(function (a, b) {
                 return 0.5 - Math.random();
               }),
               clickedCat: [],
@@ -62,27 +62,27 @@ class App extends Component {
     }
   }
 
-//the order of components to be rendered: navbar, jumbotron, friendcard, footer
-render() {
-  return (
-    <div>
-      <Navbar
-        score={this.state.score}
-      />
-      <Jumbotron />
-      <div className="wrapper">
-        {this.state.cat.map(fish => (
-          <FriendCard
-            imageClick={this.imageClick}
-            id={cat.id}
-            key={cat.id}
-            image={cat.image}
-          />
-        ))}
+  //the order of components to be rendered: navbar, jumbotron, friendcard, footer
+  render() {
+    return (
+      <div>
+        <Navbar
+          score={this.state.score}
+        />
+        <Jumbotron />
+        <div className="wrapper">
+          {this.state.cat.map(fish => (
+            <FriendCard
+              imageClick={this.imageClick}
+              id={cat.id}
+              key={cat.id}
+              image={cat.image}
+            />
+          ))}
+        </div>
+        {/* <Footer /> */}
       </div>
-      <Footer />
-    </div>
-  );
-}
+    );
+  }
 }
 export default App;
