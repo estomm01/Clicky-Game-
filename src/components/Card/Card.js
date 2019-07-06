cbbc445ca9b291a854eeb78068c92d0f39b2081b
Markus Maasikas <markus@hotmail.com>
2019-07-05 21:45:26 -0400

    updated js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,12 +5,12 @@ import "./Card.css";
 
 //pass the image into each card so all 12 are rendered
 const Card = props => (
-  <div className="Card col-md-4" onClick={props.imageClick}>
+    <div className="Card col-md-3 mt-3" onClick={props.imageClick}>
     <div className="img-container">
-
-      <img alt={props.image.replace(".jpg", "")} src={require("../../images/" + props.image)} />
+      <img alt={props.image.replace(".jpg", "",)} src={require("../../images/" + props.image)} />
     </div>
   </div>
 );
 
 export default Card;
+//"Card col-md-4"
